8e03144d8bebb8e30258b7ad25bc5a35da7cf814
fbessagu <francois.bessaguet@gmail.com>
2017-07-23 16:25:38 +0200

    Update from Pro PC

--- app/src/main/java/com/example/dclone/myvocabapps/MyMainVocabActivity.java
+++ app/src/main/java/com/example/dclone/myvocabapps/MyMainVocabActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
+ * Update from another PCs
  */
 public class MyMainVocabActivity extends AppCompatActivity {
     /**
